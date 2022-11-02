2dfa2075418fed9841977a12a567a4e5a469b97f
mcarricerutti <martinacarri345@gmail.com>
2022-11-02 09:12:06 -0300

    Creación page Notes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import CarpNotas from "./components/CarpNotas";
+import Files from "./components/Files";
+import Notes from "./components/Notes";
+
+function App() {
+
+  return (
+    <>
+    <Files/>
+    <Notes/>
+    <CarpNotas/>
+    </>
+  );
+}
+
+export default App;
